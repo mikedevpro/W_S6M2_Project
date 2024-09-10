4a32a00cb132472f9ece3089f63a366ca79364ad
Michael Nobles <Mnobles33@gmail.com>
2024-09-10 10:07:39 -0700

    Final Commit :-)

--- frontend/components/Posts/Posts.js
+++ frontend/components/Posts/Posts.js
@@ -3,12 +3,15 @@ import Post from './Post';
 
 const Posts = (props) => {
   // 🔥 Make sure the parent of Posts is passing the right props!
-  const { likePost, posts } = props;
+  const { posts, likePost } = props;
 
   return (
     <div className='posts-container-wrapper'>
       {/* Map through the posts array returning a Post component at each iteration */}
       {/* Check the implementation of Post to see what props it requires! */}
+      {
+        posts.map(pst => <Post likePost={likePost} post={pst} key={pst.id} />)
+      }
     </div>
   );
 };
